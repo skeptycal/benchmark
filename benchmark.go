6d7e443fc3242f6084bcffca5947fec3f799319a
skeptycal <26148512+skeptycal@users.noreply.github.com>
2021-05-04 02:01:01 -0500

    GoBot: setup repo files from template

---
+++ benchmark.go
@@ -0,0 +1,6 @@
+// Copyright (c) 2021 Michael Treanor
+// https://github.com/skeptycal
+// MIT License
+
+// Package benchmark contains utilities for macOS.
+package benchmark
--- idea.md
+++ idea.md
@@ -1,4 +1,4 @@
-# gorepotemplate
+# benchmark
 
 > Tricky and fun utilities for Go programs on macOS.
 
